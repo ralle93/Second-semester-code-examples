54b64381db9cc126e5b8e5902c686bb2d62be698
Rasmus Thrane <snubi68@gmail.com>
2017-06-08 10:40:17 +0200

    SQL info added

--- src/JavafxGeneral.java
+++ src/JavafxGeneral.java
@@ -6,7 +6,7 @@ public class JavafxGeneral {
     When you've created a stage object you need to apply a scene object to that stage in order to be able to add other Objects to your GUI. The scene object will then act as a lower level 'container' which contains objects for your GUI. If you want to create a scene Object you have to import javafx.scene.Scene;
 
     JavaFX Node (fx Containers som VBox, HBox, TableView, StackPane, men også Controls som Label, Button)
-    Hvilken struktur holder sammen på Node objekter?
+    Hvilken struktur holder sammen på Node objekter? -
     Eventhandler - en metode der først bliver kørt, når eventet sker (fx en knap, der trykkes på)
     Lambda udtryk - lambdaer er forkortelser af en anonym klasse der kun har én metode
     Layout typer - også Containers som VBox, HBox, GridPane
