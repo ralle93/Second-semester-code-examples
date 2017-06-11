6dff7fef92066b37821360b728c115508776bc0a
Rasmus Thrane <snubi68@gmail.com>
2017-06-11 16:22:08 +0200

    SQL info added

--- src/SearchingAndSortingAndIterators.java
+++ src/SearchingAndSortingAndIterators.java
@@ -1,2 +1,14 @@
 public class SearchingAndSortingAndIterators {
+
+
+   /**
+    *
+    * Gif på hvordan de forskellige sorting algorimter fungere
+    * http://imgur.com/gallery/fq0A8hx
+    *
+    *
+    *
+    *
+    *
+    */
 }
--- src/Singleton.java
+++ src/Singleton.java
@@ -27,7 +27,7 @@ public class Singleton {
                }
             return instance;
              }
-         }
+            }
     *
     *
     *
