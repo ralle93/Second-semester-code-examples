39fad449be8871c17c393ababb785ad7dddfd09d
Rasmus Thrane <snubi68@gmail.com>
2017-06-11 16:10:02 +0200

    SQL info added

--- src/TreemapHashmapTreeSetHashset.java
+++ src/TreemapHashmapTreeSetHashset.java
@@ -1,2 +1,36 @@
 public class TreemapHashmapTreeSetHashset {
+   /**
+    *
+    *    TreeMap
+    *
+    *
+    *
+    *
+    *
+    *    HashMap
+    *
+    *    a hashmap holds a key and a variable or a object, the key is used to map the the value it holds.
+    *    HashMap is a simlpe and powerfull way to store data. used to get data faster than normal Lists since it searches the key and
+    *    not the
+    *    Permits null values
+    *    can hold duplicate values
+    *
+    *
+    *    TreeSet
+    *    same as treemap, can't hold duplicate values
+    *
+    *    HashSet
+    *
+    *    same as Hashmap, but can't hold duplicate values
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    */
 }
